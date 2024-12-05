60febb1e12e7301f23c4208357c987572dd4cd88
sharifuzzaman24 <private.sharifuzzaman@gmail.com>
2024-12-05 12:54:37 +0600

    base project created

---
+++ src/pages/AllMoviesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllMoviesPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllMoviesPage;
---
+++ src/pages/UpdateMoviePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateMoviePage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UpdateMoviePage;
